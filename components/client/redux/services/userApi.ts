0e38f9f97bfe9a5b4a44f790da9cf17c79bf0529
yairyakobson <yairnet92@gmail.com>
2026-03-17 15:09:19 +0200

    new: RTK Query user endpoints

---
+++ components/client/redux/services/userApi.ts
@@ -0,0 +1,72 @@
+"use client"
+
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+import {
+  setIsAuthenticated,
+  setIsLoading,
+  setUser
+} from "../features/userSlice";
+import { UserQueryData } from '../../clientInterfaces/userQueryInterface';
+
+export const userAPI = createApi({
+  reducerPath: "userAPI",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "/api/v1/user"
+  }),
+  tagTypes: ["User"],
+  endpoints: (builder) =>({
+    getCurrentUser: builder.query<UserQueryData, void>({
+      query: () => "/me",
+      providesTags: ["User"],
+      async onQueryStarted(_, { dispatch, queryFulfilled }){
+        try{
+          const { data } = await queryFulfilled;
+          dispatch(setUser(data))
+          dispatch(setIsAuthenticated(true));
+        }
+        catch(error){
+          dispatch(setIsLoading(false));
+          console.log(error);
+        }
+        finally{
+          dispatch(setIsLoading(false));
+        }
+      }
+    }),
+    updatePassword: builder.mutation({
+      query(body){
+        return{
+          url: "/update/password",
+          method: "PUT",
+          body
+        }
+      }
+    }),
+    forgotPassword: builder.mutation({
+      query(body){
+        return{
+          url: "/password/forgot",
+          method: "POST",
+          body
+        }
+      }
+    }),
+    resetPassword: builder.mutation({
+      query({ token, body }){
+        return{
+          url: `/password/reset/${token}`,
+          method: "PUT",
+          body
+        }
+      }
+    })
+  })
+});
+
+export const {
+  useGetCurrentUserQuery,
+  useUpdatePasswordMutation,
+  useForgotPasswordMutation,
+  useResetPasswordMutation
+} = userAPI;
