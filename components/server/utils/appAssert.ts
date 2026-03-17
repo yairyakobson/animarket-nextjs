a164903ee51c7e6bdf42ab27ea1d03ed41e2f742
yairyakobson <yairnet92@gmail.com>
2026-03-17 15:30:59 +0200

    new: global error message structure file

---
+++ components/server/utils/appAssert.ts
@@ -0,0 +1,12 @@
+import assert from "node:assert";
+
+import AppAssert from "../types/appAssertTypes";
+import AppError from "./appError";
+
+const appAssert: AppAssert = (
+  condition,
+  httpStatusCode,
+  message
+) => assert(condition, new AppError(httpStatusCode, message));
+
+export default appAssert;
