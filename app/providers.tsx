d295003d00d0982c155757e99a405032dbc61145
yairyakobson <yairnet92@gmail.com>
2026-03-17 14:53:41 +0200

    new: Redux providers file

---
+++ app/providers.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useRef } from "react";
+import { Provider } from "react-redux";
+
+import { store } from "@/components/client/redux/store";
+import { UserProviderProps } from "@/components/client/clientInterfaces/userProviderInterface";
+import {
+  setIsAuthenticated,
+  setIsLoading,
+  setUser
+} from "@/components/client/redux/features/userSlice";
+
+export default function Providers({ children, user }: UserProviderProps){
+  const initialized = useRef(false);
+
+  if(!initialized.current){
+    if(user){
+      store.dispatch(setUser(user));
+      store.dispatch(setIsAuthenticated(true));
+      store.dispatch(setIsLoading(false));
+    }
+    else{
+      store.dispatch(setIsLoading(false));
+    }
+    initialized.current = true;
+  }
+  
+  return(
+    <Provider store={store}>
+      {children}
+    </Provider>
+  );
+}
