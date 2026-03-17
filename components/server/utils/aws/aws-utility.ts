b168a552df988f75b7fd67872e5e0ac81ede0490
yairyakobson <yairnet92@gmail.com>
2026-03-17 15:42:03 +0200

    new: AWS S3 client configuration

---
+++ components/server/utils/aws/aws-utility.ts
@@ -0,0 +1,14 @@
+import { S3Client } from "@aws-sdk/client-s3";
+import {
+  PRIMARY_AWS_REGION,
+  AWS_ACCESS_KEY_ID
+  ,AWS_SECRET_ACCESS_KEY
+} from "../../constants/env-keys";
+
+export const s3Client = new S3Client({
+  region: PRIMARY_AWS_REGION,
+  credentials: {
+    accessKeyId: AWS_ACCESS_KEY_ID,
+    secretAccessKey: AWS_SECRET_ACCESS_KEY
+  }
+});
