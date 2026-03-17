595e1453c681a0c8ef0c78332b5bd192a0d4a58a
yairyakobson <yairnet92@gmail.com>
2026-03-17 15:39:34 +0200

    new: JWT configuration file

---
+++ components/server/utils/jwt/jwtToken.ts
@@ -0,0 +1,38 @@
+import { NextResponse } from "next/server";
+
+import { COOKIE_EXPIRATION_TIME, NODE_ENV } from "../../constants/env-keys";
+
+import { CookieOptions } from "../../serverInterfaces/cookieInterface";
+
+export async function sendToken(
+  user: CookieOptions,
+  statusCode: number,
+  payload: Record<string, any> = {}
+){
+  const token = user.getJwtToken(); // Create JWT Token
+
+  const cookieExpirationDays = Number(COOKIE_EXPIRATION_TIME) || 7; // fallback: 7 days
+  const expires = new Date(Date.now() + cookieExpirationDays * 24 * 60 * 60 * 1000);
+
+  const response = NextResponse.json({
+    ...payload,
+    user: {
+      _id: user._id.toString(),
+      name: user.name,
+      email: user.email
+    },
+    token
+  }, { status: statusCode });
+
+  response.cookies.set({
+    name: "token",
+    value: token,
+    httpOnly: true,
+    expires,
+    path: "/", // ensure it's available app-wide
+    secure: NODE_ENV === "production",
+    sameSite: "strict"
+  });
+
+  return response;
+}
