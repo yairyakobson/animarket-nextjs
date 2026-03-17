0f56f4e605f59051df7d8b797cfd9d8fce4ddc79
yairyakobson <yairnet92@gmail.com>
2026-03-17 15:09:49 +0200

    new: Redux slice file

---
+++ components/client/redux/features/userSlice.ts
@@ -0,0 +1,35 @@
+"use client"
+
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+import type { UserQueryData } from "../../clientInterfaces/userQueryInterface";
+import type { UserStateData } from "../../clientInterfaces/userStateData";
+
+const initialState: UserStateData = {
+  user: null,
+  isAuthenticated: false,
+  loading: true
+}
+
+export const userSlice = createSlice({
+  name: "userSlice",
+  initialState,
+  reducers: {
+    setUser: (state, action: PayloadAction<UserQueryData | null>) =>{
+      state.user = action.payload
+    },
+    setIsAuthenticated: (state, action: PayloadAction<boolean>) =>{
+      state.isAuthenticated = action.payload
+    },
+    setIsLoading: (state, action: PayloadAction<boolean>) =>{
+      state.loading = action.payload
+    }
+  }
+});
+
+export const {
+  setUser,
+  setIsAuthenticated,
+  setIsLoading
+} = userSlice.actions
+export default userSlice.reducer;
