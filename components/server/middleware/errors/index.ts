c5164e64e9c5f336ac97df9363ef788daa52c586
yairyakobson <yairnet92@gmail.com>
2026-05-03 19:31:15 +0300

    modified: deleted MongoDB related error

--- components/server/middleware/errors/index.ts
+++ components/server/middleware/errors/index.ts
@@ -1,11 +1,9 @@
-import { mongooseCastError } from "./castError";
 import { duplicateError } from "./duplicateError";
 import { expiredJwtError } from "./expiredJwtError";
 import { invalidJwtError } from "./invalidJwtError";
 
 export const knownErrorHandlers = [
   duplicateError,
-  mongooseCastError,
   invalidJwtError,
   expiredJwtError
 ];
