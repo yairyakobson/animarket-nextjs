b8dacb318f8d80fc119897af73e1e5e4263d7bb7
yairyakobson <yairnet92@gmail.com>
2026-05-03 19:29:44 +0300

    new: Drizzle helper functions for passwords

--- components/server/dataAccess/password.ts
+++ components/server/dataAccess/password.ts
@@ -1,27 +1,78 @@
-import crypto from "crypto";
+import { and, eq, gt, sql } from "drizzle-orm";
 
-import User from "../schemas/mongoose/User";
-import verificationModel from "../schemas/mongoose/VerificationCode";
+import { db } from "@/drizzle-utils/main-config";
+import { users } from "@/drizzle-utils/schemas/user-schema";
+import { verificationCodes } from "@/drizzle-utils/schemas";
 
 export const findUserPassword = async(email: string) =>{
-  return await User.findOne({ email });
+  const [userPasswordFinderResult] = await db
+  .select()
+  .from(users)
+  .where(eq(users.email, email))
+  .limit(1);
+  
+  return userPasswordFinderResult;
 }
 
-export const updateResettedPassword = async(plainToken: string) =>{
-  const hashedToken = crypto
-  .createHash("sha256")
-  .update(plainToken)
-  .digest("hex");
+export const resetPasswordRecord = async(query: {
+  userId: string;
+  resetPasswordToken: string;
+  resetPasswordExpire: Date;
+}) =>{
+  const [resetPasswordRecordResult] = await db
+  .update(users)
+  .set({
+    resetPasswordToken: query.resetPasswordToken,
+    resetPasswordExpire: query.resetPasswordExpire
+  })
+  .where(eq(users.id, query.userId))
+  .returning()
 
-  return await User.findOne({
-    resetPasswordToken: hashedToken,
-    resetPasswordExpire: { $gt: Date.now() }
-  }).select("+password");
+  return resetPasswordRecordResult;
+}
+
+export const updateResettedPassword = async(token: string) =>{
+  const [resetPasswordResult] = await db
+  .select()
+  .from(users)
+  .where(
+    and(
+      eq(users.resetPasswordToken, token),
+      gt(users.resetPasswordExpire, sql`now()`)
+    )
+  )
+  .limit(1);
+
+  return resetPasswordResult;
+};
+
+export const hashResettedPassword = async(
+  hashedNewPassword: string,
+  user: { id: string }
+) =>{
+  const [hashedPasswordResult] = await db
+  .update(users)
+  .set({
+    password: hashedNewPassword,
+    resetPasswordToken: null,
+    resetPasswordExpire: null
+  })
+  .where(eq(users.id, user.id))
+  .returning();
+
+  return hashedPasswordResult;
 };
 
 export const deleteResetPasswordEntry = async(query: {
-  name: string,
+  userId: string,
   type: string
 }) =>{
-  return await verificationModel.deleteOne(query);
+  return await db
+  .delete(verificationCodes)
+  .where(
+    and(
+      eq(verificationCodes.userId, query.userId),
+      eq(verificationCodes.type, query.type)
+    )
+  );
 }
