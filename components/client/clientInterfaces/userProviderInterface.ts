1876e95f61ece82baeb8f32d6a05bbb4a01233b2
yairyakobson <yairnet92@gmail.com>
2026-05-08 16:16:49 +0300

    small refactor

--- components/client/clientInterfaces/userProviderInterface.ts
+++ components/client/clientInterfaces/userProviderInterface.ts
@@ -1,12 +1,6 @@
+import { UserQueryData } from "./userQueryInterface";
+
 export interface UserProviderProps{
   children: React.ReactNode;
-  user?: {
-    id: string;
-    name: string;
-    email: string;
-    avatar: string;
-    public_id: string;
-    url: string;
-    signed_url: string;
-  } | null;
+  user?: UserQueryData | null;
 }
