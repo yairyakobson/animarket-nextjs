da331727ea8d91e214595a1506841179c4a95885
yairyakobson <yairnet92@gmail.com>
2026-05-08 16:23:07 +0300

    new: new sidebar component

--- components/client/layout/UserSidebar.tsx
+++ components/client/layout/UserSidebar.tsx
@@ -1,36 +1,25 @@
-import { Col, Container, Row } from "react-bootstrap";
-
-import { getServerUser } from "@/components/server/utils/cookies/clientCookie";
-
 import SidebarConfig from "./SidebarConfig";
-
-import sidebarStyles from "../styles/sidebar.module.scss";
 
 export default async function UserSidebar(
   { children }: { children: React.ReactNode })
 {
-  const user = await getServerUser()
-
-  const menuItems = [
-    { name: "Overview", url: `/profile/${user?.name}` },
-    { name: "Settings", url: "/profile/settings" }
-  ];
-
   return(
     <>
       <section>
-        <h1 className={sidebarStyles.userSidebar}>
-          {`${user?.name}'s Sidebar`}
-        </h1>
+        <section className="drawer drawer-open">
+          <section className="drawer-toggle"/>
+          <section className="drawer-content mt-[1.5rem] py-[1rem]">
+            <section>{children}</section>
+          </section>
 
-        <Container as="section">
-          <Row as="section">
-            <Col as="section" lg={3}>
-              <SidebarConfig menuItems={menuItems}/>
-            </Col>
-            <Col as="section" lg={9}>{children}</Col>
-          </Row>
-        </Container>
+          <section className="drawer-side">
+            <section className="block h-screen bg-gray-200">
+              <ul className="menu p-4">
+                <SidebarConfig/>
+              </ul>
+            </section>
+          </section>
+        </section>
       </section>
     </>
   );
