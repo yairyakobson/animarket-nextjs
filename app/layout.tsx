a021d82bbb50a59cb300fd2a9376667b1381ef5b
yairyakobson <yairnet92@gmail.com>
2026-05-08 16:15:47 +0300

    modified: fixed global styling issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,15 +21,20 @@ export default async function RootLayout(
 
   return(
     <>
-      <html lang="en">
+      <html lang="en" data-scroll-behavior="smooth">
         <body>
           <Providers user={user}>
-            <Navbar isAuthenticated={isAuthenticated}/>
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css"
-            rel="stylesheet"
-            integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr"
-            crossOrigin="anonymous"/>
-              {children}
+            <section className="flex flex-col min-h-dvh">
+              <Navbar isAuthenticated={isAuthenticated}/>
+              <main className="flex flex-col flex-grow">
+                <link 
+                href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css"
+                rel="stylesheet"
+                integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr"
+                crossOrigin="anonymous"/>
+                {children}
+              </main>
+            </section>
           </Providers>
           <Toasters/>
         </body>
