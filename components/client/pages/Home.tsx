3fe28627325fd61684a74155ef89336a0edd150c
yairyakobson <yairnet92@gmail.com>
2026-05-08 16:20:57 +0300

    modified: homepage content issue resolved

--- components/client/pages/Home.tsx
+++ components/client/pages/Home.tsx
@@ -1,4 +1,4 @@
-"use client";
+"use client"
 
 import { Button } from "react-bootstrap";
 
@@ -6,20 +6,22 @@ import { HomeProps } from "../clientInterfaces/pages/homepageProps";
 
 export default function Home({ username }: HomeProps){
   return(
-    <section className="hero min-h-screen bg-red-400">
-      <section className="hero-overlay !flex !flex-col !items-center !max-w-fill">
-        <h1 className="mt-5 !text-5xl font-bold !text-neutral-content text-center">
-          {`Welcome to Animarket ${username}`}
-        </h1>
-        <p className="mt-5 !text-zinc-300 text-center max-w-xl">
-          Your one-stop shop for everything Anime
-        </p>
-        <section>
-          <Button className="mt-5 btn btn-primary" href="/upload">
-            Get Started
-          </Button>
+    <>
+      <section className="hero flex-grow bg-gray-200">
+        <section className="hero-overlay bg-gray-500 flex flex-col !items-center !max-w-fill">
+          <h1 className="mt-5 !text-5xl font-bold text-center">
+            {`Welcome to Animarket ${username}`}
+          </h1>
+          <p className="mt-5 text-center max-w-xl">
+            Your one-stop shop for everything Anime
+          </p>
+          <section>
+            <Button className="mt-5 btn btn-danger !shadow-none" href="/signup">
+              Get Started
+            </Button>
+          </section>
         </section>
       </section>
-    </section>
+    </>
   );
 }
