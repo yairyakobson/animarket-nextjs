cbe10bd17e73f341e2b009e066e0dcd609052b1f
yairyakobson <yairnet92@gmail.com>
2026-05-12 15:12:10 +0300

    modified: deleted interface

--- components/client/clientInterfaces/pages/uploadImageProps.ts
+++ components/client/clientInterfaces/pages/uploadImageProps.ts
@@ -3,14 +3,3 @@ import { UserQueryData } from "../userQueryInterface";
 export interface UploadImageProps{
   initialUser: UserQueryData | null;
 }
-
-export interface UploadImageFunctions extends UploadImageProps{
-  fileRef?: React.RefObject<{
-    public_id: string;
-    image: File | null;
-  }>;
-  
-  fileHandler: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  handleUpload: () => Promise<any>;
-  handleDelete: () => Promise<any>;
-}
