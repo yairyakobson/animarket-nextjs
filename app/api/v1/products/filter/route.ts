a149af6fba247472d74de8af3edea41adf336fac
yairyakobson <yairnet92@gmail.com>
2026-05-17 12:25:49 +0300

    new: filter products API route

---
+++ app/api/v1/products/filter/route.ts
@@ -0,0 +1,40 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { BAD_REQUEST, NOT_FOUND, OK } from "@/components/server/constants/httpCodes";
+import { zodFilterProductSchema } from "@/components/server/schemas/zod/zod-product/ZodProductFilter";
+import { filterProducts } from "@/components/server/dataAccess/products";
+import errorHandler from "@/components/server/middleware/errorHandler";
+
+export async function GET(req: NextRequest){
+  try{
+    const { searchParams } = new URL(req.url);
+    const queryObj = Object.fromEntries(searchParams.entries());
+
+    const parsed = zodFilterProductSchema.safeParse(queryObj);
+      
+    if(!parsed.success){
+      return NextResponse.json({
+        error: parsed.error.issues
+      }, { status: BAD_REQUEST });
+    }
+
+    const filteredProducts = await filterProducts(parsed.data);
+    console.log(`Number of products: ${filteredProducts.length}`)
+
+    if(filteredProducts.length > 0){
+      return NextResponse.json(
+        { message: `Number of products: ${filteredProducts.length}`,
+          filteredProducts
+         }, 
+        { status: OK }
+      );
+    }
+    return NextResponse.json(
+      { message: "No products found" }, 
+      { status: NOT_FOUND }
+    );
+  }
+  catch(error){
+    return errorHandler(req, error);
+  }
+}
