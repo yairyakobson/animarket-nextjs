1db9fa61abfa888ae2820dbab85c67227b9bcf43
yairyakobson <yairnet92@gmail.com>
2026-05-25 00:33:37 +0300

    modified: changed imports for sidebar pages

--- app/profile/settings/page.tsx
+++ app/profile/settings/page.tsx
@@ -1,8 +1,8 @@
 import { getServerUser } from "@/components/server/utils/cookies/clientCookie";
 
 import UserSidebar from "@/components/client/layout/UserSidebar";
-import ChangePassword from "@/components/client/pages/settings/ChangePassword";
-import UploadProfileImage from "@/components/client/pages/settings/UploadProfileImage";
+import ChangePassword from "@/components/client/pages/sidebar/ChangePassword";
+import UploadProfileImage from "@/components/client/pages/sidebar/UploadProfileImage";
 
 export default async function UpdateUserPage(){
   const user = await getServerUser();
