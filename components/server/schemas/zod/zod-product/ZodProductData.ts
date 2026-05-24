e5eeff33c5e110914c1ebfd2d01976baf1175af8
yairyakobson <yairnet92@gmail.com>
2026-05-25 00:26:49 +0300

    modified: modified products Zod schema

--- components/server/schemas/zod/zod-product/ZodProductData.ts
+++ components/server/schemas/zod/zod-product/ZodProductData.ts
@@ -1,19 +1,19 @@
 import z from "zod";
+
 import { zodSellerSchema } from "./ZodSeller";
+import { zodProductImageSchema } from "./ZodProductImage";
 
 export const zodProductDataSchema = z.object({
   name: z.string().min(1, "Name is required"),
   description: z.string(),
-  price:z.coerce.number().positive(),
+  price: z.coerce.number().positive(),
+  stock: z.coerce.number().nonnegative(),
   category: z.string(),
-  condition: z.enum(["New", "Like New", "Refurbished"]),
-  stock: z.coerce.number().int().nonnegative(),
-  public_id: z.string().optional(),
-  url: z.url().optional(),
-  signed_url: z.url().optional(),
-  totalReviews: z.number().int().default(0),
-  averageRating: z.number().default(0)
+  condition: z.string(),
+  totalReviews: z.string().default("0"),
+  averageRating: z.string().default("0")
 })
 .extend({
-  seller: zodSellerSchema
+  seller: zodSellerSchema,
+  image: zodProductImageSchema
 });
