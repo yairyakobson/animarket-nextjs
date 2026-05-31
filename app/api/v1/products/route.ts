c7501423b6e7ae5064b7d59cf10fa148b68ddb21
yairyakobson <yairnet92@gmail.com>
2026-05-31 16:13:27 +0300

    small rafactoring

--- app/api/v1/products/route.ts
+++ app/api/v1/products/route.ts
@@ -5,15 +5,16 @@ import { BAD_REQUEST, OK } from "@/components/server/constants/httpCodes";
 import { fetchAllProducts } from "@/components/server/dataAccess/products";
 
 export async function GET(){
-  try{
-    const products = await fetchAllProducts();
+  try {
+    const productsList = await fetchAllProducts();
     return NextResponse.json(
-      products,
+      productsList,
       { status: OK }
     );
   }
   catch(error){
     console.error("Fetching error:", error);
+
     return NextResponse.json({
       error: "Failed to fetch products"
     }, { status: BAD_REQUEST });
