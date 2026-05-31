c139a489c1f52f94bb84840591a97bbe1406ed19
yairyakobson <yairnet92@gmail.com>
2026-05-31 15:58:08 +0300

    new: product review helper functions

---
+++ components/server/dataAccess/reviews.ts
@@ -0,0 +1,71 @@
+import { desc, eq, sql } from "drizzle-orm";
+
+import { db } from "@/drizzle-utils/main-config";
+import { NewReview, products, reviews } from "@/drizzle-utils/schemas";
+
+export const submitNewProductReview = async(
+  newReview: NewReview
+) =>{
+  const [newReviewResult] = await db
+  .insert(reviews)
+  .values(newReview)
+  .onConflictDoUpdate({
+    target: [reviews.productId, reviews.reviewer],
+    set: {
+      rating: newReview.rating,
+      comment: newReview.comment,
+      createdAt: new Date()
+    }
+  })
+  .returning();
+
+  if(newReviewResult){
+    await db
+    .update(products)
+    .set({
+      totalReviews: sql`(
+      SELECT count(*)::int 
+      FROM ${reviews} 
+      WHERE ${reviews.productId} = ${newReviewResult.productId})`,
+      
+      averageRating: sql`(
+      SELECT coalesce(round(avg(${reviews.rating})::numeric, 1), 0)::float 
+      FROM ${reviews} 
+      WHERE ${reviews.productId} = ${newReviewResult.productId})`,
+      })
+    .where(eq(products.id, newReviewResult.productId));
+  }
+
+  return newReviewResult;
+}
+
+export const fetchProductReviews = async(query: string) =>{
+  const reviewFetcherResult = await db
+  .select()
+  .from(reviews)
+  .where(
+    eq(reviews.productId, query)
+  )
+  .limit(1);
+  
+  return reviewFetcherResult;
+}
+
+export const productReviews = async(query: string) =>{
+  const productreviewsTable = await db
+  .select()
+  .from(reviews)
+  .where(eq(reviews.productId, query))
+  .orderBy(desc(reviews.createdAt));
+  
+  return productreviewsTable;
+}
+
+export const sellerReviewSubmissionBlocker = async(productId: string) =>{
+  const [productSellerFinder] = await db
+  .select({ seller: products.seller })
+  .from(products)
+  .where(eq(products.id, productId));
+
+  return productSellerFinder;
+}
