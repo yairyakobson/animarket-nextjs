eb656fa2d6d76675a03217b9cb9d6a3e9a73fcc2
yairyakobson <yairnet92@gmail.com>
2026-05-31 16:13:05 +0300

    new: reviews RTK Query endpoints

---
+++ components/client/redux/services/reviewApi.ts
@@ -0,0 +1,30 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const reviewAPI = createApi({
+  reducerPath: "reviewAPI",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "/api/v1"
+  }),
+  tagTypes: ["Product", "Reviews"],
+  endpoints: (builder) =>({
+    submitReview: builder.mutation({
+      query(body){
+        return{
+          url: "/reviews/new",
+          method: "PUT",
+          body
+        }
+      },
+      invalidatesTags: ["Product"]
+    }),
+    getProductReviews: builder.query({
+      query: (productId) => `/reviews/${productId}`,
+      providesTags: ["Reviews"]
+    })
+  })
+});
+
+export const {
+  useSubmitReviewMutation,
+  useGetProductReviewsQuery
+} = reviewAPI;
