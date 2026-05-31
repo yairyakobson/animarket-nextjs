f2bae6cd2d9b786155d94ac04c16e1e9e3814d23
yairyakobson <yairnet92@gmail.com>
2026-05-31 16:06:52 +0300

    modified: added reviews display component

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,9 +1,17 @@
+import { fetchProductReviews } from "@/components/server/dataAccess/reviews";
+import { ProductDetailsProps } from "@/components/server/serverInterfaces/productInterface";
+
+import Reviews from "@/components/client/layout/Review/Reviews";
 import ProductDetails from "@/components/client/pages/ProductDetails";
 
-export default async function ProductPage(){
+export default async function ProductPage({ params }: ProductDetailsProps){
+  const productId = params.productId
+  const reviews = await fetchProductReviews(productId);
+
   return(
     <>
       <ProductDetails/>
+      <Reviews productReviews={reviews}/>
     </>
   );
 }
