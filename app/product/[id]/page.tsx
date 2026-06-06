f5efe96b33283bb4c096445e938d9379774f8375
yairyakobson <yairnet92@gmail.com>
2026-06-06 18:43:39 +0300

    modified: reviews section removed

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,17 +1,9 @@
-import { fetchProductReviews } from "@/components/server/dataAccess/reviews";
-import { ProductDetailsProps } from "@/components/server/serverInterfaces/productInterface";
-
-import Reviews from "@/components/client/layout/Review/Reviews";
 import ProductDetails from "@/components/client/pages/ProductDetails";
 
-export default async function ProductPage({ params }: ProductDetailsProps){
-  const productId = params.productId
-  const reviews = await fetchProductReviews(productId);
-
+export default async function ProductPage(){
   return(
     <>
       <ProductDetails/>
-      <Reviews productReviews={reviews}/>
     </>
   );
-}
+};
