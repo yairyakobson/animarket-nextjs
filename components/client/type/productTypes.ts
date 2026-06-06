e3085c29c566ff75ac16a0ece550260b02a5d5f1
yairyakobson <yairnet92@gmail.com>
2026-06-06 18:51:07 +0300

    new: custom type for products

---
+++ components/client/type/productTypes.ts
@@ -0,0 +1,10 @@
+type productsType = {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  url: string;
+  totalReviews: number;
+  averageRating: number;
+}
