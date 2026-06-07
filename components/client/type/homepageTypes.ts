d5b6d6b2a73773181095ab9d7202aba8c1fac128
yairyakobson <yairnet92@gmail.com>
2026-06-07 21:54:40 +0300

    delete: deleted un-necessary files

--- components/client/type/homepageTypes.ts
+++
@@ -1,3 +0,0 @@
-type UsernameProps = {
-  username: string;
-}
--- components/client/type/zodErrorType.ts
+++
@@ -1,7 +0,0 @@
-type ZodErrorResponse = {
-  error: {
-    fieldErrors?: Record<string, string[]>;
-    formErrors?: string[];
-  };
-  statusCode?: number;
-};
