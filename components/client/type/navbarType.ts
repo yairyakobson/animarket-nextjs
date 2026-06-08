38f0655f56d9cee79caba6a6a10ef8107e0062a2
yairyakobson <yairnet92@gmail.com>
2026-06-08 14:46:43 +0300

    new: navbar custom type is now exported globally

--- components/client/type/navbarType.ts
+++ components/client/type/navbarType.ts
@@ -1,12 +1,6 @@
-type NavbarProps = {
+import { UserQueryData } from "../clientInterfaces/userStateInterfaces/userQueryData";
+
+export type NavbarProps = {
   isAuthenticated?: boolean;
-  user?: {
-    _id: string;
-    name: string;
-    email: string;
-    avatar: string;
-    public_id?: string;
-    url?: string;
-    signed_url?: string;
-  } | null;
+  user?: UserQueryData | null
 };
