8fa48d777ed063d9b42a6df87760de3ce849a46b
yairyakobson <yairnet92@gmail.com>
2026-06-12 16:28:02 +0300

    new: added an additional utility timeframe function

--- components/server/utils/date.ts
+++ components/server/utils/date.ts
@@ -23,3 +23,6 @@ export const thirtyDaysFromNow = () =>
   new Date(Date.now() + 30 * 24 * 60 * 60 * 1000);
 
 export const ONE_DAY_MS = 24 * 60 * 60 * 1000;
+
+export const oneWeekAgo = () =>
+  new Date(Date.now() - 7 * 24 * 60 * 60 * 1000);
