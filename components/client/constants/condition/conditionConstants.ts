56e5405ee2cb777a3a5e92f34f7e97d93ee004dc
yairyakobson <yairnet92@gmail.com>
2026-06-18 16:33:47 +0300

    modified: renamed the file and changed its structure

---
+++ components/client/constants/condition/conditionConstants.ts
@@ -0,0 +1,7 @@
+export enum PRODUCT_CONDITION{
+  New = "New",
+  LikeNew = "Like New",
+  Refurbished = "Refurbished"
+}
+
+export const Condition = Object.values(PRODUCT_CONDITION);
--- components/client/constants/condition/productConstants.ts
+++
@@ -1,5 +0,0 @@
-export const PRODUCT_CONDITION = [
-  "New",
-  "Like New",
-  "Refurbished"
-]
