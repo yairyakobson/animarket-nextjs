7058e8b97ccbd6002ef9ac52119790df1f5462b4
yairyakobson <yairnet92@gmail.com>
2026-06-25 16:51:07 +0300

    modified: added product category key

--- components/client/clientInterfaces/pageInterfaces/homepageProps.ts
+++ components/client/clientInterfaces/pageInterfaces/homepageProps.ts
@@ -1,7 +1,9 @@
-import { LatestProductsProps } from "../../type/product/latestProps";
 import { TopRatedProductsProps } from "../../type/product/topRatedProps";
+import { LatestProductsProps } from "../../type/product/latestProps";
+import { CategoriesProps } from "../../type/product/categoryProps";
 
 export interface HomeProps{
   topRatedProducts: TopRatedProductsProps[];
   latestProducts: LatestProductsProps[];
+  productCategories: CategoriesProps[];
 }
