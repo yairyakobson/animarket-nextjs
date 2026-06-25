6cc720f50a1e44fe48536da82aaa8c150818b163
yairyakobson <yairnet92@gmail.com>
2026-06-25 16:58:14 +0300

    modified: added function for fetching category list

--- components/server/dataAccess/filteredProducts.ts
+++ components/server/dataAccess/filteredProducts.ts
@@ -1,19 +1,7 @@
-import { desc, eq, gte } from "drizzle-orm";
+import { asc, desc, eq, gte } from "drizzle-orm";
 
 import { db } from "@/drizzle-utils/main-config";
 import { products } from "@/drizzle-utils/schemas";
-
-export const fetchLatestProducts = async(date: Date) =>{
-  const newProductsResult = await db
-  .select()
-  .from(products)
-  .where(
-    gte(products.createdAt, date)
-  )
-  .orderBy(desc(products.createdAt));
-  
-  return newProductsResult;
-}
 
 export const fetchTopRatedProducts = async(minRating: number) =>{
   const topRatedProductsResult = await db
@@ -27,13 +15,35 @@ export const fetchTopRatedProducts = async(minRating: number) =>{
   return topRatedProductsResult;
 }
 
+export const fetchLatestProducts = async(date: Date) =>{
+  const newProductsResult = await db
+  .select()
+  .from(products)
+  .where(
+    gte(products.createdAt, date)
+  )
+  .orderBy(desc(products.createdAt));
+  
+  return newProductsResult;
+}
+
+export const fetchCategoryList = async() =>{
+  const fetchCategoriesResult = await db
+  .select({ name: products.category })
+  .from(products)
+  .groupBy(products.category)
+  .orderBy(asc(products.category));
+  
+  return fetchCategoriesResult.map(row => row.name);
+}
+
 export const fetchCategorizedProducts = async(category: string) =>{
-  const categorizedProducts = await db
+  const fetchedCategorizedProducts = await db
   .select()
   .from(products)
   .where(
     eq(products.category, category)
   );
   
-  return categorizedProducts;
+  return fetchedCategorizedProducts;
 }
