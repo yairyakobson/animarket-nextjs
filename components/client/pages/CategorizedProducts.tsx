a2ca4b61a4312ccae98e5167f4c250daa7cbf899
yairyakobosn <yairnet92@gmail.com>
2026-07-09 17:37:53 +0300

    change: changed the file to a function component

--- components/client/pages/CategorizedProducts.tsx
+++ components/client/pages/CategorizedProducts.tsx
@@ -8,42 +8,42 @@ import { ProductRating } from "../layout/ProductRating";
 
 import userProductsStyles from "../styles/userStyles/userProducts.module.scss";
 
-const CategorizedProducts = ({ categoryProducts }: CategorizedProductsMapping) =>{
+const CategorizedProducts: React.FC<CategorizedProductsMapping> = ({ categoryProducts }) =>{
   return(
     <> 
       <Container className={userProductsStyles.userProductsContainer}>
         {categoryProducts?.map((categoryProduct) =>(
           <section key={categoryProduct?.id} className="card w-[15rem] h-[15rem] mt-[0.5rem]">
-              <figure className="relative w-full h-[12.5rem] bg-neutral">
-                <Image
-                src={categoryProduct?.url as string || "/placeholder.webp"}
-                alt={categoryProduct?.name}
-                className="w-full h-full"/>
-              </figure>
+            <figure className="relative w-full h-[12.5rem] bg-neutral">
+              <Image
+              src={categoryProduct?.url as string || "/placeholder.webp"}
+              alt={categoryProduct?.name}
+              className="w-full h-full"/>
+            </figure>
 
-              <section className="card-body justify-between">
-                <h5 className="card-title">
-                  <Link href={`/product/${categoryProduct?.id}`}
-                  className="text-decoration-none text-black">{categoryProduct?.name}</Link>
-                </h5>
-                <section className="flex items-center gap-2">
-                  <ProductRating
-                  rating={categoryProduct?.averageRating}
-                  className="1.5rem"
-                  isReadOnly={true}/>
-                  <Col as="section" className="leading-none transform -translate-y-[1.5px]">
-                    ({categoryProduct?.averageRating})
-                  </Col>
-                </section>
+            <section className="card-body justify-between">
+              <h5 className="card-title">
+                <Link href={`/product/${categoryProduct?.id}`}
+                className="text-decoration-none text-black">{categoryProduct?.name}</Link>
+              </h5>
+            <section className="flex items-center gap-2">
+              <ProductRating
+              rating={categoryProduct?.averageRating}
+              className="1.5rem"
+              isReadOnly={true}/>
+              <Col as="section" className="leading-none transform -translate-y-[1.5px]">
+                ({categoryProduct?.averageRating})
+              </Col>
+            </section>
 
-                <section className="card-actions items-center justify-end">
-                  <section className={`${userProductsStyles.priceBadge} badge-outline`}>
-                    {"$" + categoryProduct?.price}
-                  </section>
-                </section>
+            <section className="card-actions items-center justify-end">
+              <section className={`${userProductsStyles.priceBadge} badge-outline`}>
+                {"$" + categoryProduct?.price}
               </section>
             </section>
-          ))}
+          </section>
+         </section>
+      ))}
       </Container>
     </>
   );
