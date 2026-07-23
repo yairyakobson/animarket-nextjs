78bcee0fb6cf2d14163305e6d9393c2b68de0f38
yairyakobosn <yairnet92@gmail.com>
2026-07-23 15:19:25 +0300

    new: search bar sidebar component

---
+++ components/client/layout/SearchedProductsSidebar.tsx
@@ -0,0 +1,21 @@
+import SearchbarConfig from "./SearchbarConfig";
+
+export default function SearchedProductsSidebar()
+{
+  return(
+    <>
+      <section>
+        <section className="drawer drawer-open">
+          <section className="drawer-toggle"/>
+            <section className="drawer-side">
+              <section className="block h-screen bg-gray-200">
+                <ul className="menu p-4">
+                  <SearchbarConfig/>
+                </ul>
+            </section>
+          </section>
+        </section>
+      </section>
+    </>
+  );
+}
